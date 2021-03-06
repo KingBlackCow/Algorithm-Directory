c4f58694211b73fb1f86414978ff9ef914600738
sgs11 <sgs1159@naver.com>
2021-03-07 02:48:36 +0900

    greedy

---
+++ src/Greedy/BOJ14659_한조서열정리하고옴ㅋㅋ.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int ary[]=new int[n];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ary[i]=Integer.parseInt(st.nextToken());
+        }
+        int ans=0;
+        int tmp=0;
+        for (int i = 0; i < n; i++) {
+            tmp=0;
+            for (int j = i+1; j < n; j++) {
+                if(ary[i]>ary[j]){
+                    tmp++;
+                }else{
+                    break;
+                }
+            }
+            ans=Math.max(tmp,ans);
+
+        }
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+
+
+
+}
