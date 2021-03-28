3b7a568621474801a77fa9ed02fda6add670a501
KingBlackCow <sgs1159@naver.com>
2021-03-28 16:16:16 +0900

    relocate

--- src/구현/BOJ18870_좌표압축.java
+++ src/구현/BOJ18870_좌표압축.java
@@ -1,8 +1,13 @@
+package 구현;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
-class Main4 {
+class BOJ18870_좌표압축 {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
