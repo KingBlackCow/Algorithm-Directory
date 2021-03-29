93ed5f6b8aabb0b61021583bce0bfb5d20634c61
KingBlackCow <sgs1159@naver.com>
2021-03-29 19:26:51 +0900

    DP

---
+++ src/Dijkstra/BOJ1735_최단경로2.java
@@ -0,0 +1,84 @@
+package Dijkstra;
+
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1735_최단경로2 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final int INF = Integer.MAX_VALUE;
+    static int v,e,k;
+    static List<Node>[] list;
+    static int[] dist;
+
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(br.readLine());
+        list = new ArrayList[v + 1];
+        dist = new int[v + 1];
+
+        Arrays.fill(dist, INF);
+
+        for(int i = 1; i <= v; i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int i = 0 ; i < e; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list[start].add(new Node(end, weight));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        dijkstra(k);
+        for(int i = 1; i <= v; i++){
+            if(dist[i] == INF) sb.append("INF\n");
+            else sb.append(dist[i] + "\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static void dijkstra(int start){
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        boolean[] visit = new boolean[v + 1];
+        q.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while(!q.isEmpty()){
+            Node tmp = q.poll();
+            int cnt = tmp.end;
+
+//            if(visit[cnt] == true) continue;
+//            visit[cnt] = true;
+
+            for(Node node : list[cnt]){
+                if(dist[node.end] > dist[cnt] + node.weight){
+                    dist[node.end] = dist[cnt] + node.weight;
+                    q.add(new Node(node.end, dist[node.end]));
+                }
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node>{
+        int end, weight;
+
+        public Node(int end, int weight){
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return weight - o.weight;
+        }
+    }
+}
--- src/Dijkstra/BOJ4485_녹색옷입은애가젤다지.java
+++ src/Dijkstra/BOJ4485_녹색옷입은애가젤다지.java
@@ -8,37 +8,11 @@ import java.util.StringTokenizer;
 
 public class BOJ4485_녹색옷입은애가젤다지 {
 
-
-    static class Node implements Comparable<Node> {
-
-        int r, c, weight;
-
-        public Node(int row, int col, int cost) {
-
-            this.r = row;
-            this.c = col;
-            this.weight = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
-
-    }
-
-    static int N;
+    static int n;
     static int[][] map;
     static int[][] D;
     static int[] dy = {0, 1, -1, 0};
     static int[] dx = {1, 0, 0, -1};
-
-    // 범위 검사
-    static boolean isValid(int x, int y) {
-        if (x < 0 || x >= N || y < 0 || y >= N)
-            return false;
-        return true;
-    }
 
     public static int dijkstra() {
         PriorityQueue<Node> pq = new PriorityQueue<>();
@@ -49,31 +23,31 @@ public class BOJ4485_녹색옷입은애가젤다지 {
             for (int i = 0; i < 4; i++) {
                 int r = tmp.r + dx[i];
                 int c = tmp.c + dy[i];
-                if (r < 0 || c < 0 || r >= N || c >= N) continue;
+                if (r < 0 || c < 0 || r >= n || c >= n) continue;
                 if (D[r][c] > D[tmp.r][tmp.c] + map[r][c]) {
                     D[r][c] = D[tmp.r][tmp.c] + map[r][c];
                     pq.add(new Node(r, c, D[r][c]));
                 }
             }
         }
-        return D[N-1][N-1];
+        return D[n -1][n -1];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
+        StringTokenizer st;
         int turn = 0;
         while (true) {
-            N = Integer.parseInt(br.readLine());
-            if (N == 0)
+            n = Integer.parseInt(br.readLine());
+            if (n == 0)
                 break;
-            map = new int[N][N];
-            D = new int[N][N];
+            map = new int[n][n];
+            D = new int[n][n];
 
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < n; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                     D[i][j] = Integer.MAX_VALUE;
                 }
@@ -84,6 +58,20 @@ public class BOJ4485_녹색옷입은애가젤다지 {
         }
         System.out.println(sb);
         br.close();
+    }
+
+
+    static class Node implements Comparable<Node> {
+        int r, c, weight;
+        public Node(int row, int col, int cost) {
+            this.r = row;
+            this.c = col;
+            this.weight = cost;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
     }
 
 }
