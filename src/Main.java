4d147794cbe9dae160c097524035e9b67f75f2fd
KingBlackCow <sgs1159@naver.com>
2021-05-23 22:06:42 +0900

    dfs,bfs

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int n, s;
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        boolean[] used = new boolean[26];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        int[] ary = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ary[i] = Integer.parseInt(st.nextToken());
+        }
+        int one = 0;
+        int two = 0;
+        int sum = 0;
+        int len = Integer.MAX_VALUE;
+        while (true) {
+            if (two < ary.length) {
+                if (sum < s) {
+                    sum += ary[two];
+                    two++;
+                } else {
+                    len = Math.min(len, two - one);
+                    sum -= ary[one];
+                    one++;
+                }
+            } else {
+                if (sum >= s) {
+                    len = Math.min(len, two - one);
+                }
+                sum -= ary[one];
+                one++;
+                if(one>=ary.length)break;
+            }
+        }
+        if(len==Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(len);
 
     }
-
 }
