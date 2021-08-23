6e7a0691d1358a5e1c0856d2048c4b3213e114f2
KingBlackCow <sgs1159@naver.com>
2021-08-23 21:11:16 +0900

    그리디

---
+++ src/Greedy/BOJ18310_안테나.java
@@ -0,0 +1,27 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ18310_안테나 {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        long[] ary = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            ary[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ary);
+
+
+        System.out.println(ary[(n-1)/2]);
+    }
+}
+
