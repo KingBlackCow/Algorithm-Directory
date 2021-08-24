b91b1918878dd480de0cda7ea6cc7aaff5673da0
KingBlackCow <sgs1159@naver.com>
2021-08-24 18:28:48 +0900

    dp

--- src/DP/BOJ14916_거스름돈.java
+++ src/DP/BOJ14916_거스름돈.java
@@ -17,9 +17,9 @@ public class BOJ14916_거스름돈 {
             System.out.println(fiveNum);
             System.exit(0);
         }
-        for (int i = fiveNum; i >=1; i--) {
+        for (int i = fiveNum; i >=0; i--) {
             int tmp= n-(i*5);
-            for (int j = 1; j <= twoNum; j++) {
+            for (int j = twoNum; j >= 0; j--) {
                 int tmp2=tmp-2*j;
                 if(tmp2==0){
                     System.out.println(i+j);
