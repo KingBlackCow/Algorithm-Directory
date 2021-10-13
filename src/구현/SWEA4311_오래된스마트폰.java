eb9410554ea94be9bd546bc5735851a638c44e43
KingBlackCow <sgs1159@naver.com>
2021-10-13 23:41:18 +0900

    구현

---
+++ src/구현/SWEA4311_오래된스마트폰.java
@@ -0,0 +1,42 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA4311_오래된스마트폰 {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static boolean[] prime;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        getPrimes(1000000);
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int cnt = 0;
+            for (int i = 2; i <= n/2; i++) {
+                if (!prime[i]&&!prime[n-i]) {
+                    cnt++;
+                }
+            }
+            sb.append(cnt +"\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    private static void getPrimes(int n) {
+        prime = new boolean[n + 1];
+        prime[0] = prime[1] = true;
+        for (int i = 2; i <= n; i++) {
+            if (!prime[i]) {
+                for (int j = i + i; j <= n; j = j + i) {
+                    prime[j] = true;
+                }
+            }
+        }
+    }
+}
